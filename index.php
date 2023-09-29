b4b79f8f37f0940b937b9903aff9c68ecca195cc
MirunaMb <mirunamarcela98@gmail.com>
2023-09-29 16:02:11 +0200

    add second object Movie's class

--- index.php
+++ index.php
@@ -24,14 +24,18 @@ class Movie
 }
 //creazione di un istanza=oggetto della classe Movie
 //new Movie() e l'operazione che crea un nuovo oggetto basato sulla classe Movie
-$movie = new Movie("Il Padrino", "Francis Ford Coppola", 1972);
+$movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972);
 
+// creazione secondo oggetto della classe Movie
+$movie2 = new Movie("Titanic", "James Cameron", 1997);
 //utillizzo del metodo per avere le info sul film
 //Prendiamo il metodo getMovieInfo() che si trova nel oggetto movie e poi lo inglobiamo nella variabile $movieInfo
 //Inglobiamo tutto nella variabile $movieInfo per stampare o manipolare le informazioni che essa contiene.
-$movieInfo = $movie->getMovieInfo();
+$movieInfo1 = $movie1->getMovieInfo();
+$movieInfo2 = $movie2->getMovieInfo();
 
 //stampiamo le informazioni
-echo $movieInfo
+echo "Film 1: $movieInfo1 <br> ";
+echo "Film 2 : $movieInfo2 ";
 
-    ?>
+?>
